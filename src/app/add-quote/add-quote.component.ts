123a1e1732e48a368110974e4b9c7b10c5f05e3e
LincolnLidanya <lincolnlidanya@gmail.com>
2020-10-13 03:30:11 +0300

    Add submitquote method and new quote instantiation

--- src/app/add-quote/add-quote.component.ts
+++ src/app/add-quote/add-quote.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Quote } from './../shared/quote.model';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-add-quote',
   templateUrl: './add-quote.component.html',
-  styleUrls: ['./add-quote.component.css']
+  styleUrls: ['./add-quote.component.css'],
 })
 export class AddQuoteComponent implements OnInit {
+  newquote = new Quote('', '', '', new Date(), 0, 0);
 
-  constructor() { }
+  @Output() addQuote = new EventEmitter<Quote>();
 
-  ngOnInit(): void {
+  submitQuote() {
+    this.addQuote.emit(this.newquote);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
