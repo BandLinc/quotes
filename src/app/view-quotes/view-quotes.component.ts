b71c49589f5a9f90982ad6aa97b533b19663219b
LincolnLidanya <lincolnlidanya@gmail.com>
2020-10-13 03:31:53 +0300

    Add the addd new quote method in the view quotes class

--- src/app/view-quotes/view-quotes.component.ts
+++ src/app/view-quotes/view-quotes.component.ts
@@ -1,15 +1,19 @@
+import { Quote } from './../shared/quote.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-view-quotes',
   templateUrl: './view-quotes.component.html',
-  styleUrls: ['./view-quotes.component.css']
+  styleUrls: ['./view-quotes.component.css'],
 })
 export class ViewQuotesComponent implements OnInit {
+  quotes: Quote[] = [new Quote('', '', '', new Date(), 0, 0)];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  addNewQuote(quotes) {
+    this.quotes.push(quotes);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
