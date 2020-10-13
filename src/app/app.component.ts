13b48567db03c588347efbe18cf76eab8512b5bb
LincolnLidanya <lincolnlidanya@gmail.com>
2020-10-13 03:38:49 +0300

    Modify to include the AddNewQuote and DeleteQuote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { Quote } from './shared/quote.model';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Quotes';
+  loadedFeature: string = 'view-quotes';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+
+  quotes: Quote[] = [];
+
+  addNewQuote(quotes) {
+    this.quotes.push(quotes);
+  }
+
+  deleteQuote(index) {
+    this.quotes.splice(index, 1);
+  }
 }
